301558191a2befaad1826ab773048bc5662939c2
sunnyfur <polin.makarova@mail.ru>
2023-02-02 10:58:57 +0500

    feat: add dialogModal

---
+++ my-app/src/Components/UI/DialogModal.tsx
@@ -0,0 +1,34 @@
+import { Dialog, IconButton, Paper } from '@mui/material'
+import { Box } from '@mui/system'
+import { ReactNode } from 'react'
+import CloseIcon from '@mui/icons-material/Close'
+import styles from './dialogModal.module.scss'
+interface PropsDialogModal {
+  onClose: () => void
+  isOpen: boolean
+  children: ReactNode
+}
+
+export const DialogModal = ({ onClose, isOpen, children }: PropsDialogModal) => {
+  const handleClose = () => {
+    onClose()
+  }
+  return (
+    <Dialog onClose={handleClose} open={isOpen}>
+      <Box>
+        <IconButton
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      </Box>
+      <Paper className={styles.dialog}>{children}</Paper>
+    </Dialog>
+  )
+}
