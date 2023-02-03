f3f04dd084adab46267a4c9e0b02b323f031ee1f
sunnyfur <polin.makarova@mail.ru>
2023-02-03 16:14:39 +0500

    refactor: delete comments feat: add provider for store

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import './App.css'
-import { Avatar, Button, Divider } from '@mui/material'
-
-import { LoginForm } from './Components/LoginForm/LoginForm'
+import { Avatar } from '@mui/material'
 // import { Avatar, Button } from '@mui/material';
 import StartPage from './pages/startPage/startPage'
+import { Provider } from 'react-redux'
+import setupStore from './store/store'
 
 function App() {
   // const { user } = useSelector((state: RootState) => state.loginReducer)
-
+  const store = setupStore()
   return (
-    <div className='App'>
-      <Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' color='secondary' />
-
-      {/* <Button color='primary' variant="contained" disabled>Text</Button>
-      <Button color='primary' variant="contained">Text</Button>
-      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" color='secondary' /> */}
-      <StartPage />
-    </div>
+    <Provider store={store}>
+      <div className='App'>
+        <Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' color='secondary' />
+        <StartPage />
+      </div>
+    </Provider>
   )
 }
 
