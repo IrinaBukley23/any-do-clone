2bf922dbbe6a66592343c7343d18ed6a23543e36
Polina <sunnyfur9@gmail.com>
2023-02-04 22:40:43 +0500

    feat: add time to calendar

--- my-app/src/Components/calendarBlock/datePlan.tsx
+++ my-app/src/Components/calendarBlock/datePlan.tsx
@@ -4,16 +4,19 @@ import IconButton from '@mui/material/IconButton'
 import Paper from '@mui/material/Paper'
 import Stack from '@mui/material/Stack/Stack'
 import Table from '@mui/material/Table'
+import Box from '@mui/material/Box'
 import ArrowLeftIcon from '@mui/icons-material/ArrowLeft'
 import ArrowRightIcon from '@mui/icons-material/ArrowRight'
 
 import styles from './datePlan.module.scss'
+import moment from 'moment'
 
 const generateTime = () => {
   const arr = []
+  // const date= new Date()
   for (let t = 7; t <= 20; t++) {
-    arr.push({ id: 1, time: t, task: '' })
-    arr.push({ id: 1, time: t, task: '' })
+    arr.push({ id: 1, time: moment().hour(t).minutes(0), task: '' })
+    arr.push({ id: 1, time: moment().hour(t).minutes(30), task: ' ' })
   }
   return arr
 }
@@ -21,26 +24,40 @@ const generateTime = () => {
 const DatePlan = () => {
   return (
     <Paper className={styles.aside}>
-      <Stack direction='row' spacing={2}>
-        <IconButton>
-          <ArrowLeftIcon />
-        </IconButton>
+      <Stack className={styles.content}>
+        <Stack direction='row' justifyContent='space-between' spacing={2}>
+          <IconButton>
+            <ArrowLeftIcon />
+          </IconButton>
+          <p>{moment().format('DD.MM.yyyy')}</p>
 
-        <IconButton>
-          <ArrowRightIcon />
-        </IconButton>
+          <IconButton>
+            <ArrowRightIcon />
+          </IconButton>
+        </Stack>
+        <Paper className={styles.table}>
+          <Table size='small'>
+            <TableHead></TableHead>
+            <TableBody>
+              {generateTime().map((row, index) => (
+                <TableRow key={row.id}>
+                  <TableCell>
+                    {index % 2 === 0 ? (
+                      <>
+                        <strong>{row.time.format('HH')}</strong>
+                        <sup>{row.time.format('mm')}</sup>
+                      </>
+                    ) : (
+                      <p></p>
+                    )}
+                  </TableCell>
+                  <TableCell>{row.task}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
       </Stack>
-      <Table>
-        <TableHead></TableHead>
-        <TableBody>
-          {generateTime().map((row) => (
-            <TableRow hover key={row.time}>
-              <TableCell>{row.time}</TableCell>
-              <TableCell>{row.task}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
     </Paper>
   )
 }
